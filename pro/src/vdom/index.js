1e8cc651e16b21bf000b0c57da86440e45c28efa
zflin <flinzhu@gravitylink.com>
2021-12-14 08:51:41 +0800

    FIXED 修改render处理中children报错问题

--- pro/src/vdom/index.js
+++ pro/src/vdom/index.js
@@ -1,5 +1,6 @@
 export function createElement(vm, tag, data = {}, children) {
-    return vnode(vm, tag, data, data.key, children || [], undefined)
+    console.log(tag)
+    return vnode(vm, tag, data, data.key, children, undefined)
 }
 
 export function createTextElement(vm, text) {
