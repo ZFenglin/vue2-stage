1d049ac457571facba8a8242b04b0f3b742acf8c
ZFLin <913632394@qq.com>
2022-01-03 15:25:21 +0800

    UPDATE 44vuex的基本实现

--- 05_Vuex/vuex-project/src/App.vue
+++ 05_Vuex/vuex-project/src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
     我是{{ this.$store.state.name }} {{ this.$store.state.age }} <br />
-    <!-- 年龄增加{{ this.$store.getters.myAge }} -->
-    <button @click="increment">更改年龄</button>
+    年龄增加{{ this.$store.getters.myAge }}
+    <button @click="$store.commit('changeAge', 2)">同步更改年龄</button>
+    <button @click="$store.dispatch('changeAge', 2)">异步更改年龄</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "app",
-  methods: {
-    increment() {
-      this.$store.state.age++;
-    },
-  },
 };
 </script>
 
--- 05_Vuex/vuex-project/src/store/index.js
+++ 05_Vuex/vuex-project/src/store/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+// import Vuex from 'vuex'
 import Vuex from '../vuex/index'
 
 Vue.use(Vuex)
@@ -9,8 +10,16 @@ export default new Vuex.Store({
     age: 26,
   },
   mutations: { // methods commit 同步状态更改
+    changeAge(state, payload) {
+      state.age += payload
+    }
   },
   actions: { // 异步操作，调用api接口，多次commit mutations
+    changeAge({ commit }, payload) {
+      setTimeout(() => {
+        commit('changeAge', payload)
+      }, 1000)
+    }
   },
   getters: { // 计算属性
     myAge(state) {
